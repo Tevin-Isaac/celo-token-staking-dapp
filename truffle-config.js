f9c55cb97d880ad9a4a3110cf76234caa479d1e2
Tevin-Isaac <isaactevin@yahoo.com>
2023-04-19 17:41:38 +0300

    updated contracts

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 require('babel-register');
 require('babel-polyfill');
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-const mnemonic = "awesome pear drive venture sign toast tide much hundred floor adapt attend"; // replace with your MNEMONIC
+const mnemonic = 'awesome pear drive venture sign toast tide much hundred floor adapt attend';
 
 module.exports = {
   networks: {
@@ -11,26 +11,31 @@ module.exports = {
       network_id: "*",
     },
     alfajores: {
-      provider: () => new HDWalletProvider(mnemonic, "https://alfajores-forno.celo-testnet.org"),
-      network_id: 44787,
-      gas: 5000000,
-      gasPrice: 1000000000 // 1 gwei
-    },
-
-    // contracts_directory: './src/contracts/',
-    // contracts_build_directory: './src/abis/',
-
-
-    mainnet: {
-      provider: function () {
-        return new HDWalletProvider(mnemonic, "https://forno.celo.org");
+      provider: function() {
+        return new HDWalletProvider(mnemonic,'https://alfajores-forno.celo-testnet.org');
       },
-      network_id: 42220,
-      gas: 4000000,
+      host: "https://alfajores-forno.celo-testnet.org",
+      port: 443,
+      network_id: 44787, // The network ID for Alfajores testnet
+      gas: 8000000, // Gas limit used for deployments
+      gasPrice: 1000000000, // Gas price used for transactions
       deployTimeout: 300000,
       networkCheckTimeout: 300000,
     },
   },
+  mainnet: {
+    provider: function () {
+      return new HDWalletProvider(mnemonic, "https://forno.celo.org");
+    },
+    network_id: 42220,
+    gas: 4000000,
+    deployTimeout: 300000,
+    networkCheckTimeout: 300000,
+  },
+
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+
   // Configure your compilers
   compilers: {
     solc: {
